1d6c925515cbb6b5cee916cd634399c3ffb79f8b
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-12-09 10:04:19 -0800

    Created and added tests

--- person.py
+++ person.py
@@ -8,21 +8,24 @@ class Person(object):
         # A person has an id, is_vaccinated and possibly an infection
         self._id = _id  # int
         self.is_vaccinated = is_vaccinated
-        self.infection = infection
+        self.infection = False
         self.is_alive = True
 
     """
-    Line #21 Only called if infection attribute is not None.
-    Line #23 Generates a random number between 0.0 - 1.0
-    Line #25 If the number is less than the mortality rate of 
-    the person's infection they have passed away
-    Line #30 If check_survival is not less than self.
-    They have survived the infection and are now vaccinated
+    Line #26 - Only called if infection attribute is not None.
+    Line #28 - Generates a random number between 0.0 - 1.0.
+    Random.uniform is used since it returns a floating number
+    between 0-1. This random number is generated to determine 
+    those who are sustainable of surviving.
+    Line #36 - If the number is less than the infection.mortality rate of 
+    the person's infection it can happen 3 ways. (1) They are not vaccinated
+    and they died, (2) they are vaccinated, (3) they weren't infected.
     """
 
     def did_survive_infection(self):
         if self.infection is not None:
-            check_survival = random.randint(0.0,1.0)
+            self.infection = True
+            check_survival = random.uniform(0, 1)                              # check_survival = random.randint(0.0,1.0)
             if check_survival < self.infection.mortality_rate:
                 self.is_vaccinated = False
                 self.is_alive = False
@@ -31,8 +34,8 @@ class Person(object):
                 self.is_vaccinated = True
                 return self.is_alive
         else:
+            self.is_infected = False
             return self.is_alive
-
             
 
 if __name__ == "__main__":
@@ -47,11 +50,11 @@ if __name__ == "__main__":
     # Create an unvaccinated person and test their attributes
     unvaccinated_person = Person(2, False)
     # TODO Test unvaccinated_person's attributes here... | Remember to release in terminal
-    print("-----------------------------")
+    print("_________________________________")
     print(unvaccinated_person._id)
     print(unvaccinated_person.is_vaccinated)
     print(unvaccinated_person.infection)
-    print("-----------------------------")
+    print("_________________________________")
 
     
     # Test an infected person. An infected person has an infection/virus
@@ -63,7 +66,10 @@ if __name__ == "__main__":
     # the values of each attribute
     # assert ...
 
-    # infected person
+    """
+    Tested values of each attributes of the person
+    """ 
+
     assert infected_person.id == 3
     assert infected_person.is_alive is True or False
     assert infected_person.is_vaccinated is False
@@ -74,6 +80,11 @@ if __name__ == "__main__":
     # You need to check the survival of an infected person. Since the chance
     # of survival is random you need to check a group of people. 
     # Create a list to hold 100 people. Use the loop below to make 100 people
+
+    """
+    Created supply of 100 unvaccinated individuals
+    """
+
     people = []
     for i in range(1, 100):
         # TODO Make a person with an infection
@@ -97,22 +108,34 @@ if __name__ == "__main__":
     # TODO If a person is_alive True add one to did_survive
     # TODO If a person is_alive False add one to did_not_survive
 
-    # Needs to be 0 since we need to document
+
+    """
+    Initially needs to be 0 since we need to document. For each person that
+    has survived, values are incremented by one. The same goes for those who
+    didn't
+    """
+
     survived = 0
     did_not_survive = 0
     for person in people:
-        if person.did_surivive_infection():
+        if person.did_survive_infection():
             survived += 1
         else:
             did_not_survive += 1
 
-    print(survived)
-    print(did_not_survive)
+    print("_________________________________")
+    print(f" Survived: {survived}")
+    print(f" Decease: {did_not_survive}")
 
+
+    # ______________________________________________________
+    
     # TODO When the loop is complete print your results.
     # The results should roughly match the mortality rate of the virus
     # For example if the mortality rate is 0.2 rough 20% of the people 
     # should succumb. 
+
+    # ______________________________________________________
 
     # Stretch challenge! 
     # Check the infection rate of the virus by making a group of 
